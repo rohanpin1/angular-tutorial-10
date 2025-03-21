493ad36195e3e7cf43b9b6527b77de6567dd103a
rohanpin1 <rohankumawat.pinkcity@gmail.com>
2025-03-21 14:40:43 +0530

    Template Form VAlidation

--- src/app/template-forms-basics/template-forms-basics.component.ts
+++ src/app/template-forms-basics/template-forms-basics.component.ts
@@ -1,9 +1,10 @@
+import { NgIf } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-template-forms-basics',
-  imports: [FormsModule],
+  imports: [FormsModule,NgIf],
   templateUrl: './template-forms-basics.component.html',
   styleUrl: './template-forms-basics.component.scss'
 })
